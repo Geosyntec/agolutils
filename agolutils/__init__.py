0ca32e1b7eafb6ef6aeb5f9f55519d26ee1b6c05
Austin Orr <austinorr@users.noreply.github.com>
2023-09-21 13:53:32 -0700

    bump patch version

--- agolutils/__init__.py
+++ agolutils/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Austin Orr"
 __email__ = "aorr@geosyntec.com"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from agolutils.render.render import render_docx_template as render_docx_template
 from agolutils.config.config import load_config as load_config

c1c804c17d6f3c91c9ecadac2c0e145135e123f7
Austin Orr <aorr@geosyntec.com>
2023-09-25 16:42:50 -0700

    revert comwrapper due to recursion error

--- agolutils/convert.py
+++ agolutils/convert.py
@@ -1,86 +1,10 @@
-import logging
 import sys
-import time
+
 from pathlib import Path
 from pywintypes import com_error
 
 if sys.platform == "win32":
     import win32com.client
-
-
-_DELAY = 0.05  # seconds
-_TIMEOUT = 60.0  # seconds
-
-
-def _com_call_wrapper(f, *args, **kwargs):
-    """
-    COMWrapper support function.
-    Repeats calls when 'Call was rejected by callee.' exception occurs.
-    """
-    # Unwrap inputs
-    args = [arg._wrapped_object if isinstance(arg, ComWrapper) else arg for arg in args]
-    kwargs = dict(
-        [
-            (key, value._wrapped_object)
-            if isinstance(value, ComWrapper)
-            else (key, value)
-            for key, value in dict(kwargs).items()
-        ]
-    )
-
-    start_time = None
-    while True:
-        try:
-            result = f(*args, **kwargs)
-        except com_error as e:
-            if e.strerror == "Call was rejected by callee.":
-                if start_time is None:
-                    start_time = time.time()
-                    logging.debug("Call was rejected by callee.")
-
-                elif time.time() - start_time >= _TIMEOUT:
-                    raise
-
-                time.sleep(_DELAY)
-                continue
-
-            raise
-
-        break
-
-    if isinstance(result, win32com.client.CDispatch) or callable(result):
-        return ComWrapper(result)
-    return result
-
-
-class ComWrapper(object):
-    """
-    Class to wrap COM objects to repeat calls when 'Call was rejected by callee.' exception occurs.
-    """
-
-    def __init__(self, wrapped_object):
-        assert isinstance(wrapped_object, win32com.client.CDispatch) or callable(
-            wrapped_object
-        )
-        self.__dict__["_wrapped_object"] = wrapped_object
-
-    def __getattr__(self, item):
-        return _com_call_wrapper(self._wrapped_object.__getattr__, item)
-
-    def __getitem__(self, item):
-        return _com_call_wrapper(self._wrapped_object.__getitem__, item)
-
-    def __setattr__(self, key, value):
-        _com_call_wrapper(self._wrapped_object.__setattr__, key, value)
-
-    def __setitem__(self, key, value):
-        _com_call_wrapper(self._wrapped_object.__setitem__, key, value)
-
-    def __call__(self, *args, **kwargs):
-        return _com_call_wrapper(self._wrapped_object.__call__, *args, **kwargs)
-
-    def __repr__(self):
-        return "ComWrapper<{}>".format(repr(self._wrapped_object))
 
 
 class WordDocument:
@@ -122,7 +46,7 @@ def get_Word(visible=False):
     if word is None:
         raise Exception("Unable to open MS Word")
 
-    return ComWrapper(word), was_open
+    return word, was_open
 
 
 def windows(paths):

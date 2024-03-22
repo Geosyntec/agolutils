e9d93f0c5c94ac6dc92a064de9cebf54435416a2
Austin Orr <aorr@geosyntec.com>
2024-03-22 10:36:25 -0700

    enable env flag for verify cert when building `gis`

--- agolutils/arcgis/utils.py
+++ agolutils/arcgis/utils.py
@@ -1,3 +1,6 @@
+import io
+import json
+import sys
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
 
@@ -7,24 +10,53 @@ from arcgis.gis import GIS
 from agolutils.config.config import load_config
 
 
+class RedirectStdStreams(object):
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush()
+        self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush()
+        self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+
+
+def _get_gis(dct):
+    f = io.StringIO()
+
+    with RedirectStdStreams(stdout=f, stderr=f):
+        gis_connection = GIS(
+            url=dct.get("ARCGIS_URL", None),
+            username=dct["ARCGIS_USERNAME"],
+            password=dct["ARCGIS_PASSWORD"],
+            verify_cert=json.loads(dct.get("VERIFY_CERT", "true")),
+        )
+    s = f.getvalue().splitlines()
+    for line in s:
+        if "Setting `verify_cert` to False is a security risk".lower() in line.lower():
+            continue
+        print(line)
+
+    return gis_connection
+
+
 def get_gis_from_env(env) -> GIS:
     config = dotenv_values(env)
 
-    return GIS(
-        url=config.get("ARCGIS_URL", None),
-        username=config["ARCGIS_USERNAME"],
-        password=config["ARCGIS_PASSWORD"],
-    )
+    return _get_gis(config)
 
 
 def get_gis_from_config(config: Union[Dict, str, Path]) -> GIS:
     config = load_config(config)
 
-    return GIS(
-        url=config.get("ARCGIS_URL", None),
-        username=config["ARCGIS_USERNAME"],
-        password=config["ARCGIS_PASSWORD"],
-    )
+    return _get_gis(config)
 
 
 def get_gis(

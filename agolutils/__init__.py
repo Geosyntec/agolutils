3e5e9a339289c670c21ed53fa8e6744c6b11d44b
Austin Orr <austinorr@users.noreply.github.com>
2025-03-14 12:32:24 -0700

    Update __init__.py

--- agolutils/__init__.py
+++ agolutils/__init__.py
@@ -2,4 +2,4 @@ from agolutils.config.config import load_config as load_config
 from agolutils.convert import convert as convert
 from agolutils.render.render import render_docx_template as render_docx_template
 
-__version__ = "0.2.6"
+__version__ = "0.3.0"
